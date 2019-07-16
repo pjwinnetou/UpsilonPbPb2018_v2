ee24a319c7ba161f099eccefc50c8c92ed1b85f3
JaeBeom Park <pjwinnetou@gmail.com>
2019-07-16 17:06:30 +0900

    up

--- cutsAndBinUpsilonV2.h
+++ cutsAndBinUpsilonV2.h
@@ -74,23 +74,6 @@ bool IsAcceptanceNoTrig(double pt, double eta)
 }
 
 
-// TAA Value
-double TAA1s[nCentBins1s+1] = {25.98, 20.46, 14.35, 8.66, 4.978, 2.66, 1.296, 0.5729, 0.1095, 5.607};
-double TAA2s[nCentBins2s+1] = {25.98, 20.46, 14.35, 8.66, 4.978, 2.66, 1.296, 0.5729, 0.1095, 5.607};
-double TAA3s[nCentBins3s+1] = {15.41, 1.405, 5.607};
-
-// TAA Unc 
-double TAA_unc1s[nCentBins1s+1] = {0.017, 0.017, 0.02, 0.028, 0.04, 0.058, 0.081, 0.11, 0.18, 0.089};
-double TAA_unc2s[nCentBins2s+1] = {0.017, 0.017, 0.02, 0.028, 0.04, 0.058, 0.081, 0.11, 0.18, 0.089};
-double TAA_unc3s[nCentBins3s+1] = {0.022, 0.12, 0.089};
-
-double TAA_unc1sLo[nCentBins1s+1] = {0.029, 0.03, 0.032, 0.038, 0.049, 0.066, 0.088, 0.112, 0.102, 0.034};
-double TAA_unc2sLo[nCentBins2s+1] = {0.029, 0.03, 0.032, 0.038, 0.049, 0.066, 0.088, 0.112, 0.102, 0.034};
-double TAA_unc3sLo[nCentBins2s+1] = {0.031, 0.044, 0.034};
-
-double TAA_unc1sHi[nCentBins1s+1] = {0.018, 0.019, 0.023, 0.033, 0.047, 0.068, 0.092, 0.124, 0.164, 0.028};
-double TAA_unc2sHi[nCentBins2s+1] = {0.018, 0.019, 0.023, 0.033, 0.047, 0.068, 0.092, 0.124, 0.164, 0.028};
-double TAA_unc3sHi[nCentBins2s+1] = {0.021, 0.067, 0.028};
 
 const double inel_cross_PbPb = 6740;
 const double NumberOfMBColl =  2454000000;
