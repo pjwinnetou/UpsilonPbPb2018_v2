3bf168e24d98d9ac4017746153bab09c13884eb1
JaeBeom Park <pjwinnetou@gmail.com>
2020-09-14 18:13:30 +0900

    up

--- MassFit/AllParmFreeFit/doFitUpsilon_Data_weighted.C
+++ MassFit/AllParmFreeFit/doFitUpsilon_Data_weighted.C
@@ -18,10 +18,10 @@
 
 using namespace std;
 using namespace RooFit;
-void doFitUpsilon_MC_pt050_cent090(
+void doFitUpsilon_Data_weighted(
        float ptLow=0, float ptHigh=50, 
        float yLow=0, float yHigh=2.4,
-       int cLow=0, int cHigh=180,
+       int cLow=20, int cHigh=180,
        float SiMuPtCut=3.5,
        bool recoSign=true,
        bool fixParameters=0  )
@@ -35,23 +35,23 @@ void doFitUpsilon_MC_pt050_cent090(
  
   TString SignalCB = "Double";
 
-  float massLow = 8.5; 
-  float massHigh = 10;
+  float massLow = 8; 
+  float massHigh = 14;
 
   float massLowForPlot = massLow;    
   float massHighForPlot = massHigh;
-  int   nMassBin  = (massHigh-massLow)*100;
+  int   nMassBin  = (massHigh-massLow)*10;
 
   float SiMuEtaCut = 2.4;
 
-  TFile* f1 = new TFile("/home/deathold/work/CMS/analysis/Upsilon_v2/UpsilonPbPb2018_v2/skimmedFiles/OniaTree_Skim_MC_UpsTrig_190801.root");
+  TFile* f1 = new TFile("/home/deathold/work/CMS/analysis/Upsilon_v2/UpsilonPbPb2018_v2/skimmedFiles/OniaRooDataSet_isMC0_Y1SW.root");
  
   TString recoSignString;
   if(recoSign==true) recoSignString = " && recoQQsign==0";
   else if(recoSign==false) recoSignString = " && recoQQsign!=0";
 
   TString kineLabel = getKineLabel (ptLow, ptHigh, yLow, yHigh, SiMuPtCut, cLow, cHigh) ;
-  TString kineCut = Form("pt>%.2f && pt<%.2f && abs(y)>%.2f && abs(y)<%.2f",ptLow, ptHigh, yLow, yHigh);
+  TString kineCut = Form("NumDimu == 1 && pt>%.2f && pt<%.2f && abs(y)>%.2f && abs(y)<%.2f",ptLow, ptHigh, yLow, yHigh);
   if (SiMuPtCut>0) kineCut = kineCut + Form(" && (pt1>%.2f) && (pt2>%.2f) && abs(eta1)<%.1f && abs(eta2)<%.1f", (float)SiMuPtCut, (float)SiMuPtCut, SiMuEtaCut, SiMuEtaCut );
   kineCut = kineCut + Form(" && (cBin>=%d && cBin<%d)",cLow, cHigh) + recoSignString;
   
@@ -61,7 +61,9 @@ void doFitUpsilon_MC_pt050_cent090(
   RooWorkspace *ws = new RooWorkspace("workspace");
   ws->import(*dataset);
   cout << "####################################" << endl;
-  RooDataSet *reducedDS = (RooDataSet*)dataset->reduce(RooArgSet(*(ws->var("mass")), *(ws->var("pt")), *(ws->var("y"))), kineCut.Data() );
+
+  RooDataSet *datasetW = new RooDataSet("datasetW","A sample",*dataset->get(),Import(*dataset),WeightVar(*ws->var("weight")));
+  RooDataSet *reducedDS = (RooDataSet*)datasetW->reduce(RooArgSet(*(ws->var("mass")), *(ws->var("pt")), *(ws->var("y")), *(ws->var("weight"))), kineCut.Data() );
   reducedDS->SetName("reducedDS");
   ws->import(*reducedDS);
   ws->data("reducedDS")->Print();
@@ -85,9 +87,8 @@ void doFitUpsilon_MC_pt050_cent090(
          
   int collId = kAADATA; 
   PSetUpsAndBkg initPset = getUpsilonPsets( collId, ptLow, ptHigh, yLow, yHigh, cLow, cHigh, SiMuPtCut) ; 
-  initPset.SetMCSgl();
 
-  RooRealVar    sigma1s_1("sigma1s_1","width/sigma of the signal gaussian mass DF",0.1, 0.01, 0.4);
+  RooRealVar    sigma1s_1("sigma1s_1","width/sigma of the signal gaussian mass DF",0.01, 0.01, 0.15);
   RooFormulaVar sigma2s_1("sigma2s_1","@0*@1",RooArgList(sigma1s_1,mRatio21) );
   RooFormulaVar sigma3s_1("sigma3s_1","@0*@1",RooArgList(sigma1s_1,mRatio31) );
 
@@ -97,14 +98,14 @@ void doFitUpsilon_MC_pt050_cent090(
   RooFormulaVar sigma2s_2("sigma2s_2","@0*@1",RooArgList(sigma1s_2,mRatio21) );
   RooFormulaVar sigma3s_2("sigma3s_2","@0*@1",RooArgList(sigma1s_2,mRatio31) );
   
-  RooRealVar alpha1s_1("alpha1s_1","tail shift", 2.522 , 0.5, 5.5);
+  RooRealVar alpha1s_1("alpha1s_1","tail shift", 3.522 , 1.386, 5.931);
   RooFormulaVar alpha2s_1("alpha2s_1","1.0*@0",RooArgList(alpha1s_1) );
   RooFormulaVar alpha3s_1("alpha3s_1","1.0*@0",RooArgList(alpha1s_1) );
   RooFormulaVar alpha1s_2("alpha1s_2","1.0*@0",RooArgList(alpha1s_1) );
   RooFormulaVar alpha2s_2("alpha2s_2","1.0*@0",RooArgList(alpha1s_1) );
   RooFormulaVar alpha3s_2("alpha3s_2","1.0*@0",RooArgList(alpha1s_1) );
 
-  RooRealVar n1s_1("n1s_1","power order", 1.705 , 0.5, 5.5);
+  RooRealVar n1s_1("n1s_1","power order", 1.905 , 1.4832, 4.345);
   RooFormulaVar n2s_1("n2s_1","1.0*@0",RooArgList(n1s_1) );
   RooFormulaVar n3s_1("n3s_1","1.0*@0",RooArgList(n1s_1) );
   RooFormulaVar n1s_2("n1s_2","1.0*@0",RooArgList(n1s_1) );
@@ -115,23 +116,57 @@ void doFitUpsilon_MC_pt050_cent090(
   RooFormulaVar f2s("f2s","1.0*@0",RooArgList(*f1s) );
   RooFormulaVar f3s("f3s","1.0*@0",RooArgList(*f1s) );
 
+  TFile *fMC = new TFile(Form("/home/deathold/work/CMS/analysis/Upsilon_v2/UpsilonPbPb2018_v2/MassFit/AllParmFreeFit/fitResMC/fitresults_upsilon_DoubleCB_pt%.1f-%.1f_y0.0-2.4_muPt3.5_centrality0-180.root",ptLow,ptHigh),"read");
+  RooWorkspace *wsMC = (RooWorkspace*) fMC->Get("workspace");
 
-  
-  cout << " n1s_1.getVal() = " << n1s_1.getVal() << endl;
+  double ninit = wsMC->var("n1s_1")->getVal();
+  double alphainit = wsMC->var("alpha1s_1")->getVal();
+  double sigmainit = wsMC->var("sigma1s_1")->getVal();
+  double finit = wsMC->var("f1s")->getVal();
+  double xinit = wsMC->var("x1s")->getVal();
+
+  n1s_1.setVal(ninit);
+  alpha1s_1.setVal(alphainit);
+  sigma1s_1.setVal(sigmainit);
+  f1s->setVal(finit);
+  x1s->setVal(xinit);
+
   RooCBShape* cb1s_1 = new RooCBShape("cball1s_1", "cystal Ball", *(ws->var("mass")), mean1s, sigma1s_1, alpha1s_1, n1s_1);
+  RooCBShape* cb2s_1 = new RooCBShape("cball2s_1", "cystal Ball", *(ws->var("mass")), mean2s, sigma2s_1, alpha2s_1, n2s_1);
+  RooCBShape* cb3s_1 = new RooCBShape("cball3s_1", "cystal Ball", *(ws->var("mass")), mean3s, sigma3s_1, alpha3s_1, n3s_1);
   RooCBShape* cb1s_2 = new RooCBShape("cball1s_2", "cystal Ball", *(ws->var("mass")), mean1s, sigma1s_2, alpha1s_2, n1s_2);
+  RooCBShape* cb2s_2 = new RooCBShape("cball2s_2", "cystal Ball", *(ws->var("mass")), mean2s, sigma2s_2, alpha2s_2, n2s_2);
+  RooCBShape* cb3s_2 = new RooCBShape("cball3s_2", "cystal Ball", *(ws->var("mass")), mean3s, sigma3s_2, alpha3s_2, n3s_2);
 
   RooAddPdf*  cb1s = new RooAddPdf("cb1s","Signal 1S",RooArgList(*cb1s_1,*cb1s_2), RooArgList(*f1s) );
-  
-  RooRealVar ch4_k1("ch4_k1","ch4_k1",0.2,-4,4);
-  RooRealVar ch4_k2("ch4_k2","ch4_k2",-0.1,-4,4);
-  RooChebychev *bkg = new RooChebychev("cPol1Bkg","Background",*(ws->var("mass")),RooArgSet(ch4_k1,ch4_k2));
+  RooAddPdf*  cb2s = new RooAddPdf("cb2s","Signal 2S",RooArgList(*cb2s_1,*cb2s_2), RooArgList(*f1s) );
+  RooAddPdf*  cb3s = new RooAddPdf("cb3s","Signal 3S",RooArgList(*cb3s_1,*cb3s_2), RooArgList(*f1s) );
 
-  RooRealVar *nSig1s= new RooRealVar("nSig1s"," 1S signals",0,10000000);
-  RooRealVar *nBkg= new RooRealVar("nBkg"," Bkg signals",0,1000000);
+  RooRealVar *nSig1s= new RooRealVar("nSig1s"," 1S signals",0,2000000);
+  RooRealVar *nSig2s= new RooRealVar("nSig2s"," 2S signals",-100,360000);
+  RooRealVar *nSig3s= new RooRealVar("nSig3s"," 3S signals",-100,26000);
   
+  // background : 
+  initPset.SetMCBkg();
+  double init_mu = initPset.bkg_mu ;
+  double init_sigma = initPset.bkg_sigma ;
+  double init_lambda = initPset.bkg_lambda ;
+
+  RooRealVar err_mu("#mu","err_mu",init_mu,  0, 35) ;
+  RooRealVar err_sigma("#sigma","err_sigma", init_sigma, 0,35);
+  RooRealVar m_lambda("#lambda","m_lambda",  init_lambda, 0,35);
+
+  RooGenericPdf *bkg;
+  RooGenericPdf *bkgLowPt = new RooGenericPdf("bkgLowPt","Background","TMath::Exp(-@0/@1)*(TMath::Erf((@0-@2)/(TMath::Sqrt(2)*@3))+1)*0.5",RooArgList( *(ws->var("mass")), m_lambda, err_mu, err_sigma) );
+  RooGenericPdf *bkgHighPt = new RooGenericPdf("bkgHighPt","Background","TMath::Exp(-@0/@1)",RooArgList(*(ws->var("mass")),m_lambda));
+  
+  if  (ptLow >= 6.5)        bkg = bkgHighPt ;
+  else bkg = bkgLowPt;
+
+  RooRealVar *nBkg = new RooRealVar("nBkg","fraction of component 1 in bkg",0,10000000000);  
+
   RooAddPdf* model = new RooAddPdf();
-  model = new RooAddPdf("model","1S+2S+3S + Bkg",RooArgList(*cb1s, *bkg),RooArgList(*nSig1s,*nBkg));
+  model = new RooAddPdf("model","1S+2S+3S + Bkg",RooArgList(*cb1s, *cb2s, *cb3s, *bkg),RooArgList(*nSig1s,*nSig2s,*nSig3s,*nBkg));
 
   ws->import(*model);
 
@@ -139,11 +174,12 @@ void doFitUpsilon_MC_pt050_cent090(
   RooPlot* myPlot2 = (RooPlot*)myPlot->Clone();
   ws->data("reducedDS")->plotOn(myPlot2,Name("dataOS_FIT"),MarkerSize(.8));
  
+  
   RooFitResult* fitRes2 = ws->pdf("model")->fitTo(*reducedDS,Save(), Hesse(kTRUE),Range(massLow, massHigh),Timer(kTRUE),Extended(kTRUE));
   ws->pdf("model")->plotOn(myPlot2,Name("modelHist"));
   ws->pdf("model")->plotOn(myPlot2,Name("Sig1S"),Components(RooArgSet(*cb1s)),LineColor(kOrange+7),LineWidth(2),LineStyle(2));
-  ws->pdf("model")->plotOn(myPlot2,Name("Sig1S_CB1"),Components(RooArgSet(*cb1s_1)),LineColor(kGreen-2),LineWidth(2),LineStyle(2));
-  ws->pdf("model")->plotOn(myPlot2,Name("Sig1S_CB2"),Components(RooArgSet(*cb1s_2)),LineColor(kMagenta+1),LineWidth(2),LineStyle(2));
+  ws->pdf("model")->plotOn(myPlot2,Components(RooArgSet(*cb2s)),LineColor(kOrange+7),LineWidth(2),LineStyle(2));
+  ws->pdf("model")->plotOn(myPlot2,Components(RooArgSet(*cb3s)),LineColor(kOrange+7),LineWidth(2),LineStyle(2));
   ws->pdf("model")->plotOn(myPlot2,Name("bkgPDF"),Components(RooArgSet(*bkg)),LineColor(kBlue),LineStyle(kDashed),LineWidth(2));
 
   myPlot2->SetFillStyle(4000);
@@ -160,7 +196,7 @@ void doFitUpsilon_MC_pt050_cent090(
   cout << " *** NLL : " << theNLL << endl;
   TString perc = "%";
 
-  float pos_text_x = 0.31;
+  float pos_text_x = 0.41;
   float pos_text_y = 0.78;
   float pos_y_diff = 0.056;
   float text_size = 15;
@@ -243,8 +279,6 @@ void doFitUpsilon_MC_pt050_cent090(
   int numFitPar = fitRes2->floatParsFinal().getSize();
   int ndf = nFullBinsPull - numFitPar;
 
-  cout << Form("chi^{2}/ndf : %.3f / %d ",chisq,ndf ) << endl;
-
   TLine *l1 = new TLine(massLow,0,massHigh,0);
   l1->SetLineStyle(9);
   l1->Draw("same");
@@ -254,20 +288,33 @@ void doFitUpsilon_MC_pt050_cent090(
   TH1D* outh = new TH1D("fitResults","fit result",20,0,20);
 
   outh->GetXaxis()->SetBinLabel(1,"Upsilon1S");
+  outh->GetXaxis()->SetBinLabel(2,"Upsilon2S");
+  outh->GetXaxis()->SetBinLabel(3,"Upsilon3S");
   
   float temp1 = ws->var("nSig1s")->getVal();  
   float temp1err = ws->var("nSig1s")->getError();  
+  float temp2 = ws->var("nSig2s")->getVal();  
+  float temp2err = ws->var("nSig2s")->getError();  
+  float temp3 = ws->var("nSig3s")->getVal();  
+  float temp3err = ws->var("nSig3s")->getError();  
   
   outh->SetBinContent(1,  temp1 ) ;
   outh->SetBinError  (1,  temp1err ) ;
+  outh->SetBinContent(2,  temp2 ) ;
+  outh->SetBinError  (2,  temp2err ) ;
+  outh->SetBinContent(3,  temp3 ) ;
+  outh->SetBinError  (3,  temp3err ) ;
 
   cout << "1S signal    =  " << outh->GetBinContent(1) << " +/- " << outh->GetBinError(1) << endl;
+  cout << "2S signal    =  " << outh->GetBinContent(2) << " +/- " << outh->GetBinError(2) << endl;
+  cout << "3S signal    =  " << outh->GetBinContent(3) << " +/- " << outh->GetBinError(3) << endl;
 
   setTDRStyle();
   writeExtraText = true;
   extraText = "Preliminary";
 
   CMS_lumi_massPull(pad1, 2 ,33);
+
 
   pad1->Update();
   pad2->Update();
@@ -279,9 +326,9 @@ void doFitUpsilon_MC_pt050_cent090(
   pad1->Update();
   pad2->Update();
 
-  TFile* outf = new TFile(Form("fitResMC/fitresults_upsilon_DoubleCB_%s.root",kineLabel.Data()),"recreate");
+  TFile* outf = new TFile(Form("fitRes/fitresults_upsilon_DoubleCB_%s.root",kineLabel.Data()),"recreate");
   outh->Write();
-  c1->SaveAs(Form("fitResMC/fitresults_upsilon_DoubleCB_%s.pdf",kineLabel.Data()));
+  c1->SaveAs(Form("fitRes/fitresults_upsilon_DoubleCB_%s.pdf",kineLabel.Data()));
   c1->Write();
   ws->Write();
   cout << "N, alpha, sigma1s, M0, f, X double CB for data " << endl;
